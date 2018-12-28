8bbf02e66c43abd863f908fa1cf888c6489befcf
Alex Zdatny <alexzdatny@gmail.com>
2018-12-28 21:04:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function displayEditForm() {
   let recipe = {}
   let rName = document.getElementById('name');
   let rDesc = document.getElementById('description');
-  let rIngr = document.getElementById('ingredients');
+  let rIngr = document.getElementByName('ingredients');
 
   recipe.name = rName.value;
   recipe.description = rDesc.value;
