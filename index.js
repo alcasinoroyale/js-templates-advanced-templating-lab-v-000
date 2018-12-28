ad7a6a6467db4630dc22e823a624a41ef797a918
Alex Zdatny <alexzdatny@gmail.com>
2018-12-28 20:32:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
