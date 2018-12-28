f2f76cfa11f7287e3dc85c4b2ff8a1d7380dc167
Alex Zdatny <alexzdatny@gmail.com>
2018-12-28 20:34:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function init() {
   //put any page initialization/handlebars initialization here
   let formTemplate = document.getElementById("recipe-form-template").innerHTML;
-  let formTemplateFn = Handlebars.compile(formTemplate)
+  let formTemplateFn = Handlebars.compile(formTemplate);
+  
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
 }
 
