ee7b71c342f0c8de69826ca11e683ad458684cd7
Alex Zdatny <alexzdatny@gmail.com>
2018-12-28 21:00:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function handleSubmit() {
   let recipe = {}
   let rName = document.getElementById('name');
   let rDesc = document.getElementById('description');
-  let rIngr = document.getElementById('ingredients');
+  let ingredientList = document.getElementById('ingredients');
 
   recipe.name = rName.value;
   recipe.description = rDesc.value;
