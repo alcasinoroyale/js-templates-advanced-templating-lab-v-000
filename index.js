c95edd02e5c93d6abffc8d9fb5f447a9b14430af
Alex Zdatny <alexzdatny@gmail.com>
2018-12-28 20:57:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  let formTemplate = document.getElementById("recipe-form-template").innerHTML;
-  let formTemplateFn = Handlebars.compile(formTemplate);
-  document.getElementById('main').innerHTML = formTemplateFn({ingredients: ['','','','','']});
 
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
   Handlebars.registerHelper('displayIngredient', function(ingredient) {
     return new Handlebars.SafeString('<li name="ingredients">' + ingredient + '</li>');
   })
+
+  let formTemplate = document.getElementById("recipe-form-template").innerHTML;
+  let formTemplateFn = Handlebars.compile(formTemplate);
+  document.getElementById('main').innerHTML = formTemplateFn({ingredients: ['','','','','']});
+
 }
 
 function handleSubmit() {
